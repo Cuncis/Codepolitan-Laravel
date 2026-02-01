e325a7e8b26f944f8a53ba20836952c62a7ced5c
Cuncis <38809651+Cuncis@users.noreply.github.com>
2026-02-01 19:00:45 +0700

    Add new movie list

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,47 +1,22 @@
-<html lang="en">
+@extends('app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie Index</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
-
-<body>
-
-
-    {{-- @for ($i = 0; $i < count($movies); $i++) <div>
-        <h2>{{ $movies[$i]['title'] }}</h2>
-        <p>Director: {{ $movies[$i]['director'] }}</p>
-        <p>Year: {{ $movies[$i]['year'] }}</p>
-        </div>
-
-        @endfor --}}
-
+@section('content')
+    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5">
         @foreach ($movies as $movie)
-            {{-- <p class="{{ $movie['year'] < 2010 ? 'text-red-500' : 'text-green-500' }} ">
-                Movie: {{ $movie['title'] }} - {{ $movie['year'] }}
-            </p> --}}
+            <div class="bg-gray-800 p-4 rounded-lg relative group">
+                <img src="{{ $movie['image'] }}" alt="{{ $movie['title'] }}" class="w-full rounded-md">
+                <h3 class="text-lg mt-2">{{ $movie['title'] }}</h3>
+                <p class="text-sm text-gray-400">{{ $movie['release_date'] }}</p>
 
-            @include('partials._movie', ['movie' => $movie])
+                <div class="absolute top-2 right-2 space-x-2 opacity-0 group-hover:opacity-100 transition">
+                    <button class="bg-green-600 p-1 rounded hover:bg-green-500">
+                        Edit
+                    </button>
+                    <button class="bg-red-600 p-1 rounded hover:bg-red-500">
+                        Remove
+                    </button>
+                </div>
+            </div>
         @endforeach
-
-        <p class="text-red-500 text-3xl">
-            If this is black, Tailwind is NOT loaded
-        </p>
-
-
-        {{-- @forelse ($movies as $movie)
-        <div>
-            <h2>{{ $movie['title'] }}</h2>
-            <p>Director: {{ $movie['director'] }}</p>
-            <p>Year: {{ $movie['year'] }}</p>
-        </div>
-        @empty
-        <h3>No movie found.</h3>
-        @endforelse --}}
-
-        {{-- {{ dd($config) }} --}}
-</body>
-
-</html>
+    </div>
+@endsection
